45df0b46f7171d84638b987f4822f3b0fbd8ae10
ann-ann <saddy666@gmail.com>
2024-05-22 15:00:19 +0300

    companies, turbo streams

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 class Quote < ApplicationRecord
   validates :name, presence: true
 
+  belongs_to :company
+
   scope :ordered, -> { order(id: :desc) }
+
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 end
